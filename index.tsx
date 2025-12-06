7b06eb43fd5b2b3a197d592eae47155df0124639
edgar089454 <michelbrownbr@gmail.com>
2025-12-06 16:19:48 -0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
 const rootElement = document.getElementById('root');
@@ -7,9 +6,4 @@ if (!rootElement) {
   throw new Error("Elemento #root não encontrado");
 }
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+createRoot(rootElement).render(<App />);
